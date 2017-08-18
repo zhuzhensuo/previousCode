03617ecfc93e5d6d73459be7bcbd218f4371cefb
zhuzhensuo <565296844@qq.com>
2017-08-18 17:18:02 +0800

    增删改查cookie方法

--- work/前段布局通用/basic.js
+++ work/前段布局通用/basic.js
@@ -353,6 +353,44 @@ var zzs=
 			return document.createElement(tag);
 		}
 		,
+		getCookie:function(name){
+			var cookieName=encodeURIComponent(name)+"=",
+			start=document.cookie.indexOf(cookieName),
+			cookieValue=null;
+			
+			if(start>-1){
+				var end=document.cookie.indexOf(";",start);
+				if(end==-1){
+					end=document.cookie.length;
+				}
+				cookieValue=decodeURIComponent(document.cookie.substring(start+cookieName.length,end));
+			}
+			
+			return cookieValue;
+		},
+		set:function(name,value,expires,path,domain,secure){
+			var cookieText=encodeURIComponent(name)+"="+encodeURIComponent(value);
+			if(expires instanceof Date){
+				cookieText+=";expires="+expires.toGMTString();
+			}
+			
+			if(path){
+				cookieText+=";path="+path;
+			}
+			
+			if(domain){
+				cookieText+=";domain="+domain;
+			}
+			
+			if(secure){
+				cookieText+=";secure="+secure;
+			}
+			document.cookie=cookieText;
+		
+		},
+		unset:function(name,path,domain,secure){
+			zzs.set(name,"",new Date(0),path,domain,secure);
+		},
 		isie:/msie/i.test(navigator.userAgent),
 		isie6:/msie 6/i.test(navigator.userAgent),
 		isff:/firefox/i.test(navigator.userAgent),
